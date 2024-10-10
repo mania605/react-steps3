d85531d0365a3a6eb35a976dcbf0f0513f98a9b8
mania605 <kim.minsun83@gmail.com>
2024-10-10 15:24:15 +0900

    서브 페이지 컴포넌트에 공통으로 전달한 모션Content컴포넌트 적용

--- src/components/common/MaskText.jsx
+++ src/components/common/MaskText.jsx
@@ -1,32 +1,60 @@
-import { motion } from 'framer-motion';
-import Mask from './Mask';
+import { useEffect, useState } from 'react';
+import Layout from '../common/Layout';
+import Pic from '../common/Pic';
+import Modal from '../common/Modal';
+import Content from '../common/Content';
 
-export default function MaskText({ children, duration = 0.5, delay = 0, color = '#000', style }) {
-	//component styles
-	const frameStyle = {
-		fontSize: '1.2rem',
-		fontFamily: 'orbitron',
-		color: color,
-		display: 'inline-block',
-		position: 'relative',
-		overflow: 'hidden',
-		marginBottom: 10
-	};
+export default function Gallery() {
+	console.log('Gallery Component Rendered!!');
+	const [Flickr, setFlickr] = useState([]);
+	const [ModalOpen, setModalOpen] = useState(false);
+	const [Index, setIndex] = useState(0);
 
-	//motion options
-	const spanMotion = {
-		in: { opacity: 0 },
-		on: { opacity: 1 },
-		out: { opacity: 0, transition: { delay: 0 } },
-		time: { duration: 0.01, delay: duration / 2 + delay }
-	};
+	useEffect(() => {
+		const method = 'flickr.people.getPhotos';
+		const flickr_api = import.meta.env.VITE_FLICKR_API;
+		const myID = '197119297@N02';
+		const num = 10;
+		const url = `https://www.flickr.com/services/rest/?method=${method}&api_key=${flickr_api}&user_id=${myID}&per_page=${num}&nojsoncallback=1&format=json`;
+
+		fetch(url)
+			.then(data => data.json())
+			.then(json => {
+				setFlickr(json.photos.photo);
+			});
+	}, []);
+
+	useEffect(() => {
+		document.body.style.overflow = ModalOpen ? 'hidden' : 'auto';
+	}, [ModalOpen]);
 
 	return (
-		<div style={{ ...frameStyle, ...style }}>
-			<motion.span variants={spanMotion} initial='in' animate='on' exit='out' transition={spanMotion.time}>
-				{children}
-			</motion.span>
-			<Mask duration={duration} delay={delay} color={color} />
-		</div>
+		<>
+			<Layout title={'GALLERY'}>
+				<Content delay={1.5}>
+					<section className='galleryList'>
+						{Flickr.map((data, idx) => {
+							return (
+								<article
+									key={idx}
+									onClick={() => {
+										setModalOpen(true);
+										setIndex(idx);
+									}}>
+									<Pic src={`https://live.staticflickr.com/${data.server}/${data.id}_${data.secret}_z.jpg`} className='pic' shadow />
+									<h3>{data.title}</h3>
+								</article>
+							);
+						})}
+					</section>
+				</Content>
+			</Layout>
+
+			{ModalOpen && (
+				<Modal setModalOpen={setModalOpen}>
+					<Pic src={`https://live.staticflickr.com/${Flickr[Index].server}/${Flickr[Index].id}_${Flickr[Index].secret}_b.jpg`} shadow />
+				</Modal>
+			)}
+		</>
 	);
 }
